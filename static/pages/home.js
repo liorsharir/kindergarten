5d361aa2b50ddb40c4e20ffd081c4b867d093dd7
amitlasri <amit.lasri85@gmail.com>
2024-02-21 01:38:47 +0200

    home page

--- static/pages/home.js
+++ static/pages/home.js
@@ -7,5 +7,19 @@ document.getElementById("root").innerHTML = /*html*/`
     <div id="logoHome"><img src="static/img/logoHome.png" alt="logoHome"/></div>
     <h1>ברוכים הבאים לגן אלה</h1>
     ${AboutUs(data)}
+     <div class="feature-list">
+         ${FeaturePresentation("ניהול תיק אישי", "פרטים, תיעוד ומעקב אישי", "static/img/manageKid.png")}
+         ${FeaturePresentation("שיבוץ משמרות","ניהול יומן ואילוצים לצוות הגן", "static/img/manageShifts.png")}
+         ${FeaturePresentation("ניהול אירועי הגן", "פרטים, עריכה, הוספה ללוח שנה", "static/img/manageEvents.png")}
+     </div>
 `
 
+function FeaturePresentation(title, description, imagePath) {
+    return /*html*/`
+        <div class="feature-box">
+            <img src="${imagePath}" alt="${title}"/>
+            <h2>${title}</h2>
+            <p>${description}</p>
+        </div>
+    `;
+}
