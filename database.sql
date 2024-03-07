f099ef5bd5bc4a895f70ca4be66e75be48e60a6b
amitlasri <amit.lasri85@gmail.com>
2024-03-07 18:47:06 +0200

    update users children

--- database.sql
+++ database.sql
@@ -51,21 +51,18 @@ create Table messages(
 
 
 insert INTO users (id,password, email,firstName,lastName, phone,birthday, gender,token,avatar,auth,notifications) Values
-("i15488950","123123","user1@gmail.com","שושי" ,"שושנה" ,"0505055555"   ,"" ,"female","","static/img/genericKINDERGARTNER.png","KINDERGARTNER",0 ),
-("i15488951","123123","user2@gmail.com","רחל"  ,"רחלה"  ,"0505055555"   ,"" ,"female","","","ASSISTANCE",0 ),
-("i15488952","123123","user3@gmail.com","אלה"  ,"אל"    ,"0505055555"   ,"" ,"female","","","ASSISTANCE",0 ),
-("i15488953","123123","user4@gmail.com","ירדן" ,"ירדנה" ,"0505055555"   ,"" ,"female","","","ASSISTANCE",0 );
-
+("210110110","123123","user1@gmail.com","שושי" ,"שושנה" ,"0505055555"   ,"" ,"female","","static/img/genericKINDERGARTNER.png","KINDERGARTNER",0 ),
+("220220220","123123","user2@gmail.com","רחל"  ,"רחלה"  ,"0505055555"   ,"" ,"female","","static/img/assistant1.jpeg","ASSISTANCE",0 ),
+("230330330","123123","user3@gmail.com","אלה"  ,"אל"    ,"0505055555"   ,"" ,"female","","static/img/assistant2.jpeg","ASSISTANCE",0 ),
+("240440440","123123","user4@gmail.com","ירדן" ,"ירדנה" ,"0505055555"   ,"" ,"female","","static/img/assistant2.jpeg","ASSISTANCE",0 );
 
 
 
 insert INTO children (id,firstName,lastName,birthday,gender,motherName,motherEmail,motherPhone,fatherName,fatherEmail,fatherPhone,MedicalInfo,image) Values
-    ("i0000001", "יוסי"  , "dan" , '2024-02-08',"female" ,"שושי","@@@@","055-555-5555","בוריס","@@@","055-555-5555","",""),
-    ("i0000002", "מייק"  , "dan" , '2024-02-08',"female" ,"רותי","@@@@","055-555-5555","סרגיי","@@@","055-555-5555","",""),
-    ("i0000003", "איציק" , "dan" , '2024-02-08',"male" ,"1פטריק","@@@@","055-555-5555","ולדרמיר","@@@","055-555-5555","",""),
-    ("i0000004", "איציק" , "dan" , '2024-02-08',"male" ,"2פטריק","@@@@","055-555-5555","ולדרמיר","@@@","055-555-5555","",""),
-    ("i0000005", "איציק" , "dan" , '2024-02-08',"male" ,"3פטריק","@@@@","055-555-5555","ולדרמיר","@@@","055-555-5555","",""),
-    ("i00000012", "איציק" , "dan" , '2024-02-08',"male" ,"10פטריק","@@@@","055-555-5555","ולדרמיר","@@@","055-555-5555","","");
+    ("201001001", "תמר"  , "רפאלי" , '2024-02-08',"female" ,"שושי","@@@@","055-555-5555","אסי","@@@","055-555-5555","","static/img/children1.jpeg"),
+    ("202002002", "מאי"  , "לוי" , '2024-02-08',"female" ,"רותי","@@@@","055-555-5555","דוד","@@@","055-555-5555","","static/img/children2.jpeg"),
+    ("203003003", "איציק" , "כהן" , '2024-02-08',"male" ,"יונית","@@@@","055-555-5555","יהונתן","@@@","055-555-5555","","static/img/children3.jpeg"),
+    ("205005005", "יובל" , "אופיר" , '2024-02-08',"male" ,"דליה","@@@@","055-555-5555","אביב","@@@","055-555-5555","","static/img/children4.jpeg");
 
 
 
