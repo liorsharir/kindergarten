01830c11fa934c663d7b01d8cc97854d24a95291
amitlasri <amit.lasri85@gmail.com>
2024-03-10 03:14:02 +0200

    updates-10.3

--- database.sql
+++ database.sql
@@ -63,10 +63,10 @@ insert INTO users
 
 
 insert INTO children (childID,firstName,lastName,birthday,gender,motherName,motherEmail,motherPhone,fatherName,fatherEmail,fatherPhone,MedicalInfo,image) Values
-    ("999999991", "איתי"   , "אופיר" , '2021-06-15',"male" ,"ליאור"  ,"1M@gmail.com","055-515-5555","עדי"  ,"1D@gmail.com","055-555-5551","אין מידע רלוונטי","static/img/child1.jpng"),
-    ("999999992", "אלמה"   , "אור" , '2021-10-05',"female" ,"הדר"  ,"2M@gmail.com","055-525-5555","גל"  ,"2D@gmail.com","055-555-5552","אין מידע רלוונטי","static/img/child2.jpng"),
-    ("999999993", "און" , "פרץ" , '2022-01-09',"male"   ,"מיכל","3M@gmail.com","055-535-5555","אילון","3D@gmail.com","055-555-5553","אין מידע רלוונטי","static/img/child3.jpng"),
-    ("999999994", "טאי" , "שמואל" , '2020-05-01',"male"   ,"מאי","4M@gmail.com","055-545-5555","עידן","4D@gmail.com","055-555-5554","אין מידע רלוונטי","static/img/child4.jpng");
+    ("999999991", "איתי"   , "אופיר" , '2021-06-15',"male","ליאור","1M@gmail.com","055-515-5555","עדי","1D@gmail.com","055-555-5551","אין מידע רלוונטי","static/img/child1.jpeg"),
+    ("999999992", "אלמה"   , "אור" , '2021-10-05',"female","הדר","2M@gmail.com","055-525-5555","גל","2D@gmail.com","055-555-5552","אין מידע רלוונטי","static/img/child2.jpeg"),
+    ("999999993", "און" , "פרץ" , '2022-01-09',"male","מיכל","3M@gmail.com","055-535-5555","אילון","3D@gmail.com","055-555-5553","אין מידע רלוונטי","static/img/child3.jpeg"),
+    ("999999994", "טאי" , "שמואל" , '2020-05-01',"male","מאי","4M@gmail.com","055-545-5555","עידן","4D@gmail.com","055-555-5554","אין מידע רלוונטי","static/img/child4.jpeg");
 
 
 
--- static/pages/home.js
+++ static/pages/home.js
@@ -2,7 +2,7 @@ document.title = "דף הבית"
 document.getElementById("root").innerHTML = /*html*/`
     ${Header()}   
     <div id="logoHome"><img src="static/img/logoHome.png" alt="logoHome"/></div>
-    <h1>ברוכים הבאים לגן אלה</h1>
+    <h1>ברוכים הבאים לגן ״אלה״</h1>
     ${AboutUs()}
 
     <div class="feature-list">
